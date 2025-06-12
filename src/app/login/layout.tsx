20a6feb8452bee6be9d03881f53b7adfd5209099
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2025-06-11 18:46:53 -0600

    Send to /app if already logged in

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,11 +1,18 @@
+import { auth } from "@/auth";
 import Spinner from "@/components/Spinner";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
-export default function LoginLayout({
+export default async function LoginLayout({
     children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+    const session = await auth();
+
+    if (session)
+        return redirect("/app");
+
     return (
         <Suspense fallback={<Spinner />}>
             {children}
