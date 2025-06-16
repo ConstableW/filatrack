aa29d29774e672f89dbd06a3b06a92ce29164b99
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2025-06-16 17:09:47 -0600

    Auto-redirect from landing if logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { auth } from "@/auth";
 import Button from "@/components/Button";
 import Subtext from "@/components/Subtext";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+    const session = await auth();
+
+    if (session)
+        return redirect("/app");
+
     return (<main className="absolute-center">
         <h1>Filatrack</h1>
         <Subtext>Keep track of all your filament rolls in the simplest way possible.</Subtext>
