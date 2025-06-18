77778a240b56b51277f627e5650576e3a8de7d38
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2025-06-18 17:32:46 -0600

    Landing improvements (forgot the landing page file lol)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
-import { auth } from "@/auth";
 import Button from "@/components/Button";
+import RandomizedFilament from "@/components/filament/RandomizedFilament";
 import Subtext from "@/components/Subtext";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
-    const session = await auth();
+    // const session = await auth();
 
-    if (session)
-        return redirect("/app");
+    // if (session)
+    //     return redirect("/app");
 
     return (<main className="absolute-center">
-        <h1>Filatrack</h1>
-        <Subtext>Keep track of all your filament rolls in the simplest way possible.</Subtext>
-        <a href="/login"><Button>Get Started</Button></a>
+        <div className="flex flex-row gap-2">
+            <RandomizedFilament />
+            <div>
+                <h1>Filatrack</h1>
+                <Subtext className="whitespace-pre-wrap">
+                    Keep track of all your filament rolls in the simplest way possible.{"\n"}
+                    Free, forever. No ads. All open-source.
+                </Subtext>
+                <a href="/login"><Button>Get Started</Button></a>
+            </div>
+        </div>
     </main>);
 }
