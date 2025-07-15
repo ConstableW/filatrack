3cbec7a79b3de85c0a2c36abd70d9cf8efee8122
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2025-07-15 15:06:23 -0600

    Remove print api

--- src/app/lib/db/index.ts
+++ src/app/lib/db/index.ts
@@ -1,11 +1,9 @@
 import * as analytics from "./analytics";
 import * as filament from "./filament";
 import * as settings from "./settings";
-import * as prints from "./prints";
 
 export const app = {
     analytics,
     filament,
     settings,
-    prints,
 };
